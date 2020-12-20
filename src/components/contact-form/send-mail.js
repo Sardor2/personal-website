13e4524c4f67237d474d3bb45b759affa14b6f65
unknown <you@example.com>
2020-12-20 19:50:50 +0500

    deploy ready for github

--- src/components/contact-form/send-mail.js
+++ src/components/contact-form/send-mail.js
@@ -4,16 +4,17 @@ import mailgun from 'mailgun-js';
 
 export default function sendMail(sender,subject,body) {
     console.log('sending mail');
-    const k = '72da48fdc46d53018c1acbfbccd51d14-203ef6d0-daca3a8a';
-    const domain = 'sandbox3548d9c5e53946edaa3ca53c9267f4ad.mailgun.org';
+    const k = 'your api key from mailgun';
+    const domain = 'domain from mailgun';
     const mg = mailgun({apiKey:k,domain:domain});
     const data = {
       from: sender,
-      to: 'abdurakhimov.sardor@gmail.com',
+      to: 'xxx@mail.com',
       subject: subject,
       text: body
     };
-    mg.messages().send(data, function (error, body) {
-      console.log(body);
-    });
+    // uncomment these lines for sending mail
+    // mg.messages().send(data, function (error, body) {
+    //   console.log(body);
+    // });
 }
